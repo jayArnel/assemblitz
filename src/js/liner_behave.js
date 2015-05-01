686e30b8c8e49fa48f265b6a249a8338c28a24f2
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-01 12:51:01 +0800

    added line numbers

--- src/js/liner_behave.js
+++ src/js/liner_behave.js
@@ -1,32 +1,30 @@
+window.onload = function(){
 
+
+	/*
+	 * This hook adds Line Number Functionality
+	 */
+	BehaveHooks.add(['keydown'], function(data){
+		var numLines = data.lines.total,
+			house = document.getElementsByClassName('line-nums')[0],
+			html = '',
+			i;
+		for(i=0; i<numLines; i++){
+			html += '<div>'+(i+1)+'</div>';					
+		}
+		house.innerHTML = html;
+	});
 	
-		window.onload = function(){
-
-
-			/*
-			 * This hook adds Line Number Functionality
-			 */
-			BehaveHooks.add(['keydown'], function(data){
-				var numLines = data.lines.total,
-					house = document.getElementsByClassName('line-nums')[0],
-					html = '',
-					i;
-				for(i=0; i<numLines; i++){
-					html += '<div>'+(i+1)+'</div>';					
-				}
-				house.innerHTML = html;
-			});
-			
-			var editor = new Behave({
-			
-				textarea: 		document.getElementById('demo'),
-				replaceTab: 	true,
-			    softTabs: 		true,
-			    tabSize: 		4,
-			    autoOpen: 		true,
-			    overwrite: 		true,
-			    autoStrip: 		true,
-			    autoIndent: 	true
-			});
-			
-		};
+	var editor = new Behave({
+	
+		textarea: 		document.getElementById('input'),
+		replaceTab: 	true,
+	    softTabs: 		true,
+	    tabSize: 		4,
+	    autoOpen: 		true,
+	    overwrite: 		true,
+	    autoStrip: 		true,
+	    autoIndent: 	true
+	});
+	
+};
